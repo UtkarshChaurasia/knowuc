ae5608ec765d881ef5c4c6609aa4189cf89b8838
Utkarsh Chaurasia <utkarshchaurasia@pop-os.localdomain>
2021-04-09 17:15:47 +0530

    navbar added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,12 @@
+import 'package:universal_html/html.dart' as html;
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
-import 'package:universal_html/html.dart' as html;
+import 'package:knowuc/animations/entranceFader.dart';
+import 'package:knowuc/utils/utils.dart';
 import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
+import 'package:knowuc/pages/nav_bar.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -14,8 +19,146 @@ class _HomePageState extends State<HomePage> {
   ItemScrollController _itemScrollController = ItemScrollController();
   ItemPositionsListener _itemPositionsListener = ItemPositionsListener.create();
 
+  final List<String> _sectionsName = [
+    "Home",
+    "About",
+    "Services",
+    "Projects",
+    "Contact",
+  ];
+
+  final List<IconData> _sectionsIcons = [
+    Icons.home,
+    Icons.person,
+    Icons.settings,
+    Icons.build,
+    Icons.phone,
+  ];
+
+  void _scroll(int i) {
+    _itemScrollController.scrollTo(index: i, duration: Duration(seconds: 1));
+  }
+
+  Widget sectionWidget(int i) {
+    if (i == 0) return HomePage();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      extendBodyBehindAppBar: true,
+      appBar: MediaQuery.of(context).size.width > 760
+          ? _appBarTabDesktop()
+          : AppBar(
+              backgroundColor: Colors.transparent,
+              elevation: 0.0,
+            ),
+      drawer: MediaQuery.of(context).size.width < 760 ? _appBarMobile() : null,
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: RawScrollbar(
+          controller: _scrollController,
+          thumbColor: primarycolor1,
+          thickness: 5.0,
+          child: ScrollablePositionedList.builder(
+            itemCount: 8,
+            itemScrollController: _itemScrollController,
+            itemPositionsListener: _itemPositionsListener,
+            itemBuilder: (context, index) {
+              return sectionWidget(index);
+            },
+          ),
+        ),
+      ),
+    );
   }
+
+  Widget _appBarActions(String childText, int index, IconData icon) {
+    return MediaQuery.of(context).size.width > 760
+        ? EntranceFader(
+            offset: Offset(0, -20),
+            delay: Duration(seconds: 3),
+            duration: Duration(seconds: 1),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: MaterialButton(
+                hoverColor: primarycolor1,
+                onPressed: () => _scroll(index),
+                child: Text(
+                  childText,
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          )
+        : Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: MaterialButton(
+              hoverColor: primarycolor1,
+              onPressed: () => _scroll(index),
+              child: ListTile(
+                leading: Icon(
+                  icon,
+                  color: primarycolor1,
+                ),
+                title: Text(childText),
+              ),
+            ),
+          );
+  }
+
+  Widget _appBarTabDesktop() {
+    return AppBar(
+      elevation: 0.0,
+      backgroundColor: Colors.transparent,
+      title: MediaQuery.of(context).size.width < 740
+          ? EntranceFader(
+              duration: Duration(seconds: 1),
+              offset: Offset(0, -20),
+              delay: Duration(seconds: 3),
+              child: NavBarLogo())
+          : EntranceFader(
+              offset: Offset(0, -20),
+              duration: Duration(seconds: 1),
+              delay: Duration(seconds: 3),
+              child: NavBarLogo(
+                height: MediaQuery.of(context).size.height * 0.035,
+              ),
+            ),
+      actions: [
+        for (int i = 0; i < _sectionsName.length; i++)
+          _appBarActions(_sectionsName[i], i, _sectionsIcons[i]),
+        EntranceFader(
+          offset: Offset(0, -20),
+          delay: Duration(seconds: 3),
+          duration: Duration(seconds: 1),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: MaterialButton(
+              hoverColor: primarycolor1.withAlpha(150),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0),
+                side: BorderSide(color: primarycolor1),
+              ),
+              onPressed: () {
+                html.window.open(
+                    'https://drive.google.com/file/d/119xfvbeUcKGO9BrpAH-i_HgsBzDW4ehr/view?usp=sharing',
+                    "pdf");
+              },
+              child: Text(
+                "Resume",
+                style: GoogleFonts.montserrat(
+                  fontWeight: FontWeight.w200,
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _appBarMobile() {}
 }
